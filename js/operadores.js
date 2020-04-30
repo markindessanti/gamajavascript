2f8cee0d9c4924a9f4c686a3338b2dd15c9b973d
Markin Dessanti <markindessanti@gmail.com>
2020-04-30 11:53:05 -0300

    Finalizada a Aula 02

---
+++ js/operadores.js
@@ -0,0 +1,24 @@
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+alert(operador + ' x ' + operando + ' = ' + resultado );
+
+var nome = 'Markin';
+var sobrenome = 'Dessanti';
+
+alert('Meu nome completo é ' + nome + ' ' + sobrenome + '.');
+
+var primeiroValor = 10;
+var segundoValor = 'azul';
+
+resultado = primeiroValor - segundoValor;
+
+alert(primeiroValor + ' - ' + segundoValor + ' = ' + resultado );
+
+var segundoValor = '3';
+
+resultado = primeiroValor - segundoValor;
+
+alert(primeiroValor + ' - ' + segundoValor + ' = ' + resultado );
