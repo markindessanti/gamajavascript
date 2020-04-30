f69c3ec7bae1d66d54398ee01d75786ad054a5ec
Markin Dessanti <markindessanti@gmail.com>
2020-04-30 16:24:21 -0300

    Finalizada a Aula 10 - Validador de CPF II

--- gama-no-javascript-basico/script.js
+++ gama-no-javascript-basico/script.js
@@ -1,6 +1,17 @@
 console.log('Javascript carregado!');
 
-function validar() {
+function validacao() {
 	cpf = document.getElementById('txtCpf').value;
 	console.log('Iniciando validação CPF: ' + cpf);
+	resultadoValidacao = validaCPF(cpf);
+
+	if (resultadoValidacao) {
+		document.getElementById('success').style.display = 'block';
+	} else {
+		document.getElementById('error').style.display = 'block';
+	}
+}
+
+function validaCPF(cpf) {
+	return false;
 }
