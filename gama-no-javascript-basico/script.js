ba556ef0c2c22ecfa9c93b551a9e611550b74a0a
Markin Dessanti <markindessanti@gmail.com>
2020-04-30 17:36:00 -0300

    Finalizada a Aula 11 - Validador de CPF III

--- gama-no-javascript-basico/script.js
+++ gama-no-javascript-basico/script.js
@@ -7,11 +7,26 @@ function validacao() {
 
 	if (resultadoValidacao) {
 		document.getElementById('success').style.display = 'block';
+		document.getElementById('error').style.display = 'none';
 	} else {
+		document.getElementById('success').style.display = 'none';
 		document.getElementById('error').style.display = 'block';
 	}
 }
 
 function validaCPF(cpf) {
-	return false;
+	if (cpf.length != 11) {
+		return false;
+	} else {
+		var numeros = cpf.substring(0, 9);
+		var digitos = cpf.substring(9);
+		var soma = 0
+		
+		for (let i = 10; i > 1; i--) {
+			soma += numeros.charAt(10 - i) * i;
+			// console.log(numeros.charAt(10 - i), i, numeros.charAt(10 - i) * i, soma);		
+		}
+
+		return true;
+	}
 }
